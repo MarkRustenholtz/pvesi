c38f0689c5b86e4b0ddeb3de4fbdf95de7f165bf
MarkRustenholtz <markrusten@gmail.com>
2025-10-10 17:30:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+// PV ESI service worker (static cache)
+const CACHE='pv-esi-v2';
+const ASSETS=['./','./index.html','./pv.css','./pv.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});

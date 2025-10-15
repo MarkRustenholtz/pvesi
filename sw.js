77abdc19ef410acd79ffe3e784fd4d65f8c81dbd
MarkRustenholtz <markrusten@gmail.com>
2025-10-15 03:34:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Nom du cache
+const CACHE_NAME = 'pv-esi-v1';
+
+// Fichiers à mettre en cache
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Installation du service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activation (nettoyage des anciens caches)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Interception des requêtes réseau
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});

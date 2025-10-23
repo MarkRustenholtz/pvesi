e9e17372915eaebed4129696619889246ebfaac6
MarkRustenholtz <markrusten@gmail.com>
2025-10-24 00:41:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nom du cache
-const CACHE_NAME = 'pv-esi-v19';
+const CACHE_NAME = 'pv-esi-v20';
 
 // Fichiers à mettre en cache
 const FILES_TO_CACHE = [
@@ -37,6 +37,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
